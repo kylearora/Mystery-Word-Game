0810374c71717616af12b80da33d940cfc3fd6f8
kylearora <kylearora@gmail.com>
2017-08-24 17:19:13 -0500

    Completed game and added win page an styling

--- app.js
+++ app.js
@@ -13,15 +13,50 @@ app.set("view engine", "mustache")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const mysteryWord = randomWord.split("")
+var sess = {
+  secret: "keyboard cat",
+  cookie: {},
+  saveUninitialized: true,
+  resave: true
+}
+app.use(session(sess))
+
+app.use(function(req, res, next) {
+  if (!req.session.pageLoads) {
+    req.session.pageLoads = 0
+  }
+  req.session.pageLoads += 1
+  next()
+})
+
 const guessLetterArray = []
+const guessCount = (randomWord.length + 2)
 
 app.get("/", function(req, res){
-  res.render("index", {
-    randomWord: randomWord,
-    mysteryWord: mysteryWord,
-    guessLetterArray: guessLetterArray
-  })
+  const mysteryWord = randomWord.split("")
+  let winspace = false
+  for (var i = 0; i < randomWord.length; i++) {
+    if (guessLetterArray.indexOf(randomWord[i]) >= 0) {
+      mysteryWord[i] = randomWord[i]
+    } else {
+      mysteryWord[i] = "_"
+      winspace = true
+    }
+  }
+  if (!winspace) {
+    res.redirect("/win")
+  } else {
+    res.render("index", {
+      randomWord: randomWord,
+      mysteryWord: mysteryWord,
+      guessLetterArray: guessLetterArray,
+      guessCount: guessCount
+    })
+  }
+})
+
+app.get("/win", function (req, res){
+  res.render("win")
 })
 
 app.post("/", function(req,res){
@@ -29,16 +64,6 @@ app.post("/", function(req,res){
   guessLetterArray.push(guessWord)
   res.redirect("/")
 })
-
-for (var i = 0; i < mysteryWord.length; i++) {
-  mysteryWord[i] = "_"
-  if (guessLetterArray === mysteryWord[i]) {
-  mysteryWord[i] === guessLetterArray
-  } else {
-  mysteryWord[i] === "_"
-  console.log(randomWord);
-  }
-}
 
 app.listen(3000, function(){
   console.log("Live on port 3000");
